d278253145e0d5b12c4bc75b793711d4d333629b
Anastasiya Markova <a.v.markova00@gmail.com>
2023-02-27 23:08:41 +0300

    add uncorrect solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const openingBrackets = new Set(bracketsConfig.map(([opening, _]) => opening));
+  const closingBrackets = new Map(bracketsConfig.map(([opening, closing]) => [closing, opening]));
+  
+  for (const char of str) {
+    if (openingBrackets.has(char)) {
+      stack.push(char);
+    } else if (closingBrackets.has(char)) {
+      const matchingOpeningBracket = closingBrackets.get(char);
+      if (stack.length === 0 || stack[stack.length - 1] !== matchingOpeningBracket) {
+        return false;
+      } else {
+        stack.pop();
+      }
+    }
+  }
+  
+  return stack.length === 0;
 }
